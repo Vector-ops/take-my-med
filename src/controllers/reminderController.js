0f7d49a8a93393668de73ca921c81b3d76c93d53
Vector-ops <sumith2827@gmail.com>
2023-10-23 19:30:36 +0530

    fix: fix email not sent to caretakers

--- src/controllers/reminderController.js
+++ src/controllers/reminderController.js
@@ -2,6 +2,7 @@ const createHttpError = require("http-errors");
 const Reminder = require("../models/reminder.schema");
 const User = require("../models/user.schema");
 const { scheduleJob, cancelReminder } = require("../utils/scheduleJob");
+const getCaretakers = require("../utils/getCaretakers");
 
 /**
  * @desc Set reminder
@@ -20,17 +21,18 @@ const setReminder = async (req, res, next) => {
 			recur,
 		});
 		const user = await User.findOne({ _id: req.session.userId });
+		if (!user) {
+			req.session.destroy();
+			return next(
+				createHttpError.Unauthorized("Please login and try again.")
+			);
+		}
 		user.reminders.push(reminder._id);
 		await user.save();
-		scheduleJob(
-			reminder.id,
-			title,
-			description,
-			time,
-			days,
-			recur,
-			user.email
-		);
+		const email = await getCaretakers(caretakers);
+		email.push(user.email);
+		console.log(email);
+		scheduleJob(reminder.id, title, description, time, days, recur, email);
 		res.status(200).json({ reminder, user });
 	} catch (error) {
 		console.error(error);
@@ -138,17 +140,41 @@ const deleteReminder = async (req, res, next) => {
 
 const updateReminder = async (req, res, next) => {
 	try {
+		const email = [];
 		const { title, description, caretakers, time, days, recur } = req.body;
+		const user = await User.findOne({ _id: req.session.userId });
+		if (!user) {
+			req.session.destroy();
+			return next(
+				createHttpError.Unauthorized("Please login and try again.")
+			);
+		}
+		email.push(user.email);
 		const reminder = await Reminder.findByIdAndUpdate(
 			{
 				_id: req.params.id,
 			},
 			{ title, description, caretakers, time, days, recur },
 			{ new: true, runValidators: true }
-		);
+		)
+			.populate("caretakers")
+			.exec();
 		if (!reminder) {
 			return next(createHttpError.NotFound("Reminder not found."));
 		}
+		reminder.caretakers.forEach((caretaker) => {
+			email.push(caretaker.email);
+		});
+		scheduleJob(
+			req.params.id,
+			reminder.title,
+			reminder.description,
+			reminder.time,
+			reminder.days,
+			reminder.recur,
+			email
+		);
+		console.log(email);
 		res.status(200).json(reminder);
 	} catch (error) {
 		console.error(error);
