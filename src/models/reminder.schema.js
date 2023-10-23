7d488f7c8f565ad822db7323a95550d58287e4e0
Vector-ops <sumith2827@gmail.com>
2023-10-23 17:57:00 +0530

    fix: remove user_id

--- src/models/reminder.schema.js
+++ src/models/reminder.schema.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 
 const reminderSchema = mongoose.Schema(
 	{
-		user_id: {
-			type: mongoose.Schema.Types.ObjectId,
-			required: true,
-			ref: "User",
-		},
 		title: {
 			type: String,
 			required: true,
